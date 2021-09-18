488d69d42c425c5adf3cd8d2a46d666dace610be
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-18 12:20:31 +0300

    adding request to get post by post id

--- app/request.py
+++ app/request.py
@@ -1,4 +1,4 @@
-import urllib.request,json
+# import urllib.request,json
 from . import db
 from sqlalchemy import desc
 from .models import Post, PostDetails, Category, User, Comment, CommentDetails
@@ -87,7 +87,6 @@ def get_posts_by_user_id(userid):
         upvote = post.upvote
         downvote = post.downvote
 
-        # post_comments_count = []
 
         count = 0
         if comments:
@@ -99,6 +98,40 @@ def get_posts_by_user_id(userid):
             post_id, post_date, post_detail, category, posted_by, profile_pic_path, count, upvote, downvote)
         post_details.append(post_detail_object)
         count = 0
+    return post_details
+
+def get_posts_by_post_id(pid):
+    '''
+    Function that retrieves posts by id
+    '''
+    post = Post.get_post_by_id(pid)
+    category = Category.get_category_by_id(post.category_id)
+    comments = get_comments()
+    user = User.query.filter_by(id=post.user_id).first()
+
+    post_id = post.id
+    post_date = post.created_at
+    post_detail = post.post
+    category = category.category_name
+    posted_by = user.first_name+" "+user.other_names
+    profile_pic_path = user.profile_pic_path
+    upvote = post.upvote
+    downvote = post.downvote
+
+
+    count = 0
+    if comments:
+        for comment in comments:
+            if comment.post_id == post_id:
+                count += 1
+
+    
+    post_details = PostDetails(
+        post_id, post_date, post_detail, category, posted_by, profile_pic_path, count, upvote, downvote)
+    
+
+    count = 0
+
     return post_details
 
 
