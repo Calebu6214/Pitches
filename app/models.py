b38cc351db001ed8d6f7975c2930efac303ac884
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-18 11:16:05 +0300

    adding model class for ptiches category

--- app/models.py
+++ app/models.py
@@ -60,3 +60,43 @@ class User( db.Model):
         '''
         db.session.add(self)
         db.session.commit()
+
+class Category(db.Model):
+    '''
+    Class that creates a pitch Category Object
+    '''
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='category', lazy='dynamic')
+
+    def __init__(self, category_name):
+        '''
+        Method that defines Category object properties.
+        Args: 
+            category_name: New category name
+        '''
+        self.category_name = category_name
+
+    def save_category(self):
+        '''
+        Method that saves the instance of review model
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    def get_categories():
+        '''
+        Method that retrieves pitch categories (all)
+        '''
+
+        categories = Category.query.all()
+        return categories
+
+    def get_category_by_id(cid):
+        '''
+        Method that retrieves pitch category by id
+        '''
+        category = Category.query.filter_by(id=cid).first()
+        return category
---
+++ app/request.py
@@ -0,0 +1,5 @@
+import urllib.request,json
+from . import db
+
+
+
