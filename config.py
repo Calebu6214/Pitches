749e86b7526cf2977f505e342ccd01828df6f700
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-20 21:36:19 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -15,14 +15,11 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = "calebkimutai97@gmail.com"
+    MAIL_PASSWORD = "Caleb254"
       #simple mde configurations 
     SIMPLEMDE_JS_IIFE=True
     SIMPLEMDE_USE_CDN=True
-    @staticmethod
-    def init_app():
-        pass
 
 class ProdConfig(Config):
     '''
